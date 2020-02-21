7f1e90621dad2ba3584b6d14e18094e53defce12
Gabriel Santiago <gabriel.santiago@onyou.com.br>
2020-02-21 10:09:04 -0300

    all working

--- frontend/index.js
+++ frontend/index.js
@@ -1,13 +1,3 @@
-const check = () => {
-    console.log(`Notification permission: ${Notification.permission}`)
-    if (!('serviceWorker' in navigator)) {
-      throw new Error('No Service Worker support!')
-    }
-    if (!('PushManager' in window)) {
-      throw new Error('No Push API Support!')
-    }
-}
-
 // Function to register a service worker.
 const registerServiceWorker = async () => {
     const swRegistration = await navigator.serviceWorker.register('service.js');
@@ -23,12 +13,19 @@ const requestNotificationPermission = async () => {
     // denied: user has denied the request.
     if(permission !== 'granted'){
         throw new Error('Permission not granted for Notification');
+    } else if(permission == 'granted'){
+        const swRegistration = await registerServiceWorker();
     }
 }
 
 const main = async () => { //notice I changed main to async function so that I can use await for registerServiceWorker
-    check();
-    const swRegistration = await registerServiceWorker();
+    console.log(`Notification permission: ${Notification.permission}`)
+    if (!('serviceWorker' in navigator)) {
+      throw new Error('No Service Worker support!')
+    }
+    if (!('PushManager' in window)) {
+      throw new Error('No Push API Support!')
+    }
     const permission =  await requestNotificationPermission(); // show the ask for notification popup
 }
 
